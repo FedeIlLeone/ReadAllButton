067024dcd612d71dd465515d7a270e1a5e3958ca
Fede <38290480+FedeIlLeone@users.noreply.github.com>
2023-05-07 23:29:51 +0200

    feat(types): use event handlers

--- src/components/original/SearchBar.tsx
+++ src/components/original/SearchBar.tsx
@@ -4,19 +4,21 @@ import { webpack } from "replugged";
 const { filters, waitForModule } = webpack;
 
 interface SearchBarProps {
+  "aria-label"?: string;
   autoComplete?: boolean;
   autoFocus?: boolean;
   className?: string;
   disabled?: boolean;
   iconClassName?: string;
+  inputProps?: React.ComponentPropsWithoutRef<"input">;
   isLoading?: boolean;
-  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  onBlur?: React.FocusEventHandler<HTMLInputElement>;
   onChange?: (value: string) => void;
-  onClear?: (e: React.MouseEvent<HTMLDivElement>) => void;
-  onFocus?: (e: React.FocusEvent<HTMLInputElement>) => void;
-  onKeyDown?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  onKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
-  onKeyUp?: (e: React.KeyboardEvent<HTMLInputElement>) => void;
+  onClear?: React.MouseEventHandler<HTMLDivElement>;
+  onFocus?: React.FocusEventHandler<HTMLInputElement>;
+  onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>;
+  onKeyPress?: React.KeyboardEventHandler<HTMLInputElement>;
+  onKeyUp?: React.KeyboardEventHandler<HTMLInputElement>;
   placeholder?: string;
   query?: string;
   size?: string;
